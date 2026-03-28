d2fb0860a96b28388857cf1112768e82763052a2
P Niranjan Roy <niranjan275865@gmail.com>
2026-03-28 22:32:38 +0530

    Add user input program in python

---
+++ input_output.py
@@ -0,0 +1,8 @@
+"""
+  To take input from the user,we use the input() funtion
+  Python automatically identifies whether the user entered a string or a number or list """
+  
+  # Python program showing the use of input()
+
+val = input("Enter your name:")
+print(val)
