0450f87dd38d49f04bb186869876bb6f6d0256a4
P Niranjan Roy <niranjan275865@gmail.com>
2026-03-28 22:56:07 +0530

    Add program to check input type in python

---
+++ input_type.py
@@ -0,0 +1,10 @@
+"""
+  input() is used to take input from user
+  program pause until input is given
+  By default, data received using input() is of string data type.
+  But it can be converted into the required data type by type casting """
+  
+  
+name= input("Enter name:")
+print(name)
+print("Type of name", type(name))
