e399432702869b610bd8bf3f15863e20bef4c748
P Niranjan Roy <niranjan275865@gmail.com>
2026-05-17 09:27:57 -0700

    Create armstrong_function.py
    Add function program to check Armstrong number

---
+++ Functions/armstrong_function.py
@@ -0,0 +1,12 @@
+def armstrong(n):
+    temp = n
+    total = 0
+
+    while n > 0:
+        digit = n % 10
+        total += digit ** 3
+        n = n // 10
+
+    return total == temp
+
+print(armstrong(153))
