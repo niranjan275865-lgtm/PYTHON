356496d16670ee37b9cee6b67eb08a5a1483f5fd
P Niranjan Roy <niranjan275865@gmail.com>
2026-06-29 15:44:56 +0530

    Create lines_ending_with_period.py
    Add program to count lines ending with a period

---
+++ File Handling/lines_ending_with_period.py
@@ -0,0 +1,14 @@
+file = open("sample.txt", "r")
+
+count = 0
+
+for line in file:
+
+    line = line.strip()
+
+    if line.endswith("."):
+        count += 1
+
+print("Lines Ending with Period:", count)
+
+file.close()
