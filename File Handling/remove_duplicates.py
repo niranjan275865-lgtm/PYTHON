61674fd33d4892983565e49b278705b5868333ee
P Niranjan Roy <niranjan275865@gmail.com>
2026-07-28 23:02:59 +0530

    Create remove_duplicates.py
    Add program to remove duplicate elements from a list

---
+++ File Handling/remove_duplicates.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 10, 30, 20, 40]
+
+unique = []
+
+for num in numbers:
+    if num not in unique:
+        unique.append(num)
+
+print("Original List:", numbers)
+print("List Without Duplicates:", unique)
