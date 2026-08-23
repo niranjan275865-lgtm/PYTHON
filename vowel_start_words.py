fadc1c62d93177c785e6aac9f3b59f9652ffcaf1
P Niranjan Roy <niranjan275865@gmail.com>
2026-08-23 20:42:17 +0530

    Create vowel_start_words.py
    Add program to count words starting with a vowel

---
+++ vowel_start_words.py
@@ -0,0 +1,10 @@
+sentence = input("Enter a Sentence: ")
+
+words = sentence.split()
+count = 0
+
+for word in words:
+    if word[0].lower() in "aeiou":
+        count += 1
+
+print("Words Starting with Vowel:", count)
