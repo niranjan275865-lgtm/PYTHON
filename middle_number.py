58f2d65e7ad5ee2005e59eefbcf166a17639efc6
P Niranjan Roy <niranjan275865@gmail.com>
2026-09-12 23:24:17 +0530

    Create middle_number.py
    Add Python program to find middle number among three numbers

---
+++ middle_number.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if (a >= b and a <= c) or (a <= b and a >= c):
+    middle = a
+elif (b >= a and b <= c) or (b <= a and b >= c):
+    middle = b
+else:
+    middle = c
+
+print("Middle number:", middle)
