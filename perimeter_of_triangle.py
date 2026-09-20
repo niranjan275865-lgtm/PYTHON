ea5d084e195ee755d066128c070e806e15e08b3f
P Niranjan Roy <niranjan275865@gmail.com>
2026-09-20 21:33:42 +0530

    Create perimeter_of_triangle.py
    Add Python program to calculate perimeter of square

---
+++ perimeter_of_triangle.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+perimeter = a + b + c
+
+print("Perimeter of triangle:", perimeter)
